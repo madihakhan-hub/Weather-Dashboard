b2ab0d67d63b0bf00ef42a647730ed78027c3707
madihakhan-hub <144630720+madihakhan-hub@users.noreply.github.com>
2023-10-30 14:16:12 -0500

    added weather API successfully

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,100 +1,76 @@
-var userFormEl = document.querySelector('#user-form');
-var languageButtonsEl = document.querySelector('#language-buttons');
-var nameInputEl = document.querySelector('#username');
-var repoContainerEl = document.querySelector('#repos-container');
-var repoSearchTerm = document.querySelector('#repo-search-term');
+// JavaScript to fetch both current weather and 5-day forecast
+document.getElementById("user-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  const cityName = document.getElementById("city").value;
+  fetchWeather(cityName);
+  fetchForecast(cityName);
+});
 
-var formSubmitHandler = function (event) {
-  event.preventDefault();
-
-  var city = nameInputEl.value.trim();
-
-  if (city) {
-    getUserRepos(city);
-
-    repoContainerEl.textContent = '';
-    nameInputEl.value = '';
-  } else {
-    alert('Please enter a valid city');
+document.getElementById("language-buttons").addEventListener("click", function (e) {
+  if (e.target && e.target.getAttribute("data-city")) {
+    const cityName = e.target.getAttribute("data-city");
+    fetchWeather(cityName);
+    fetchForecast(cityName);
   }
-};
+});
 
-var buttonClickHandler = function (event) {
-  var language = event.target.getAttribute('data-language');
+function fetchWeather(cityName) {
+  // Replace 'YOUR_API_KEY' with your OpenWeatherMap API key
+  const apiKey = '54ef0cfcb845848be77b6df7f7db42ca';
+  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
 
-  if (language) {
-    getFeaturedRepos(language);
+  fetch(weatherUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      // Extract and display relevant weather information
+      const temperature = data.main.temp;
+      const description = data.weather[0].description;
+      const humidity = data.main.humidity;
 
-    repoContainerEl.textContent = '';
-  }
-};
+      const weatherInfoContainer = document.getElementById("weather-info-container");
+      weatherInfoContainer.innerHTML = `
+        <h3>${cityName}</h3>
+        <p>Temperature: ${temperature}°C</p>
+        <p>Conditions: ${description}</p>
+        <p>Humidity: ${humidity}%</p>
+      `;
+    })
+    .catch((error) => {
+      console.error("Error fetching weather data:", error);
+    });
+}
 
-var getUserRepos = function (user) {
-  var apiUrl = 'https://api.github.com/users/' + user + '/repos';
+function fetchForecast(cityName) {
+  // Replace 'YOUR_API_KEY' with your OpenWeatherMap API key
+  const apiKey = '54ef0cfcb845848be77b6df7f7db42ca';
+  const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`;
 
-  fetch(apiUrl)
-    .then(function (response) {
-      if (response.ok) {
-        response.json().then(function (data) {
-          displayRepos(data, user);
-        });
-      } else {
-        alert('Error: ' + response.statusText);
+  fetch(forecastUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      const forecastContainer = document.getElementById("forecast-container");
+      forecastContainer.innerHTML = ''; // Clear any previous forecast data
+
+      // Process and display the 5-day forecast data
+      for (let i = 0; i < data.list.length; i += 8) {
+        const forecastData = data.list[i];
+        const date = new Date(forecastData.dt * 1000); // Convert timestamp to date
+
+        const temperature = forecastData.main.temp;
+        const description = forecastData.weather[0].description;
+
+        const forecastInfo = document.createElement("div");
+        forecastInfo.className = "list-item";
+        forecastInfo.innerHTML = `
+          <h3>${cityName} - ${date.toDateString()}</h3>
+          <p>Temperature: ${temperature}°C</p>
+          <p>Conditions: ${description}</p>
+        `;
+
+        forecastContainer.appendChild(forecastInfo);
       }
     })
-    .catch(function (error) {
-      alert('Unable to connect to GitHub');
+    .catch((error) => {
+      console.error("Error fetching forecast data:", error);
     });
-};
-
-var getFeaturedRepos = function (language) {
-  var apiUrl = 'https://api.github.com/search/repositories?q=' + language + '+is:featured&sort=help-wanted-issues';
-
-  fetch(apiUrl).then(function (response) {
-    if (response.ok) {
-      response.json().then(function (data) {
-        displayRepos(data.items, language);
-      });
-    } else {
-      alert('Error: ' + response.statusText);
-    }
-  });
-};
-
-var displayRepos = function (repos, searchTerm) {
-  if (repos.length === 0) {
-    repoContainerEl.textContent = 'No repositories found.';
-    return;
-  }
-
-  repoSearchTerm.textContent = searchTerm;
-
-  for (var i = 0; i < repos.length; i++) {
-    var repoName = repos[i].owner.login + '/' + repos[i].name;
-
-    var repoEl = document.createElement('div');
-    repoEl.classList = 'list-item flex-row justify-space-between align-center';
-
-    var titleEl = document.createElement('span');
-    titleEl.textContent = repoName;
-
-    repoEl.appendChild(titleEl);
-
-    var statusEl = document.createElement('span');
-    statusEl.classList = 'flex-row align-center';
-
-    if (repos[i].open_issues_count > 0) {
-      statusEl.innerHTML =
-        "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
-    } else {
-      statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-    }
-
-    repoEl.appendChild(statusEl);
-
-    repoContainerEl.appendChild(repoEl);
-  }
-};
-
-userFormEl.addEventListener('submit', formSubmitHandler);
-languageButtonsEl.addEventListener('click', buttonClickHandler);
+}
